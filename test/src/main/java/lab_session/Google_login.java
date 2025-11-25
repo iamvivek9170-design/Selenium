fc99377efc7cbac3ac36f16c103e0a9a5849dc49
iamvivek9170-design <iamvivek9170@gmail.com>
2025-11-25 09:20:53 +0530

    amezon

---
+++ test/src/main/java/lab_session/Google_login.java
@@ -0,0 +1,37 @@
+package lab_session;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class Google_login {
+
+	public static void main(String[] args) {
+		
+		
+		
+		
+		WebDriver dr = new ChromeDriver();
+		
+		
+		dr.get("https://accounts.google.com/");
+		
+		dr.findElement(By.cssSelector("[type=\"email\"]")).sendKeys("iamvivek9170@gmail.com");
+		
+		
+		Actions nex = new Actions(dr);
+		nex.moveToElement(dr.findElement(By.cssSelector("#identifierNext > div > button"))).click().build().perform();
+		
+		
+		dr.findElement(By.cssSelector("[type=\"password\"]")).sendKeys("Password"); ////// enter your password 
+		
+		
+//		div.VfPpkd-dgl2Hf-ppHlrf-sM5MNb > button[jsname="LgbsSe"]
+		
+		
+		
+	}
+	
+	
+}
